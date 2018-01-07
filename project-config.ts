9839513d613b91c676e209690d04668e4ac6e7b7
heifade <heifade@126.com>
2018-01-07 13:58:09 +0800

    1.0.0-beta4

--- project-config.ts
+++ project-config.ts
@@ -4,7 +4,7 @@ import { IProjectConfig } from './src/model/IProjectConfig';
 let projectConfig: IProjectConfig = {
   projectType: 'node',
   command: true,
-
+  documents: true,
 }
 
 export default projectConfig;
