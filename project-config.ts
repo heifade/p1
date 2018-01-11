99457c4de5491cf7fbf290d310c4b6921df5ce26
heifade <heifade@126.com>
2018-01-11 14:50:12 +0800

    no message

--- project-config.ts
+++ project-config.ts
@@ -1,10 +1,19 @@
-import { IProjectConfig } from './src/model/IProjectConfig';
+import { ProjectConfigModel } from "./src/model/projectConfig/ProjectConfigModel";
+import { CompileModel } from "./src/model/projectConfig/CompileModel";
 
-
-let projectConfig: IProjectConfig = {
-  projectType: 'node',
-  command: true,
+let projectConfig: ProjectConfigModel = {
+  projectType: "node",
+  compile: [
+    {
+      outDir: "./es/"
+    },
+    {
+      outDir: "./lib/"
+    }
+  ],
+  command: false,
   documents: true,
-}
+  sourceInclude: ["./src/**/*"]
+};
 
 export default projectConfig;
